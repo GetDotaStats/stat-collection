2b900e99a13f5a3aff4664d0472bbac660cc3ce6
jimmydorry <jimmydorry+github@gmail.com>
2014-09-19 17:42:12 +1000

    Fix bracket issue that would prevent stats from getting recorded properly

--- statcollection/scripts/vscripts/addon_game_mode.lua
+++ statcollection/scripts/vscripts/addon_game_mode.lua
@@ -3,6 +3,6 @@ require('main')
 
 -- Stat collection
 require('lib.statcollection')
-statcollection.addStats{{
+statcollection.addStats({
 	modID = 'XXXXXXXXXXXXXXXXXXX' --GET THIS FROM http://getdotastats.com/#d2mods__my_mods
-}}
+})
