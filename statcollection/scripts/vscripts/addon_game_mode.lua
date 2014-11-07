aa4af979b707ccac4355f54a8117c45ea6b1a91c
ash47 <ash.fourtyseven@gmail.com>
2014-11-07 22:33:16 +1100

    Implemented tons more stats, added auto sending YAY

--- statcollection/scripts/vscripts/addon_game_mode.lua
+++ statcollection/scripts/vscripts/addon_game_mode.lua
@@ -6,41 +6,12 @@ statcollection.addStats({
 
 print( "Example stat collection game mode loaded." )
 
-if YourGamemode == nil then
-    YourGamemode = class({})
-end
+-- Note: Stats will be sent automatically sent to the server when the game ends
+-- plesae see the top of lib/statcollection.lua for more details on how to override this behavior
 
 --------------------------------------------------------------------------------
 -- ACTIVATE
 --------------------------------------------------------------------------------
 function Activate()
-    GameRules.YourGamemode = YourGamemode()
-    GameRules.YourGamemode:InitGameMode()
-end
-
---------------------------------------------------------------------------------
--- INIT
---------------------------------------------------------------------------------
-function YourGamemode:InitGameMode()
-    local GameMode = GameRules:GetGameModeEntity()
-
-    -- Register Think
-    GameMode:SetContextThink( "YourGamemode:GameThink", function() return self:GameThink() end, 0.25 )
-
-    -- Register Game Events
-end
-
---------------------------------------------------------------------------------
-function YourGamemode:GameThink()
-    -- Check to see if the game has finished
-    if GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
-        -- Send stats
-        statcollection.sendStats()
-
-        -- Delete the thinker
-        return
-    else
-        -- Check again in 1 second
-        return 1
-    end
+    print("What an excellent gamemode :) ")
 end
