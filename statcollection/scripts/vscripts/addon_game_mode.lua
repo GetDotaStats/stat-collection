95b4dd25c1683875be65c6d4b9b6b7b0d92fdce0
jimmydorry <jimmydorry@users.noreply.github.com>
2015-03-26 13:09:50 +1000

    Extra logic to assist development

--- statcollection/scripts/vscripts/addon_game_mode.lua
+++ statcollection/scripts/vscripts/addon_game_mode.lua
@@ -1,13 +1,18 @@
 -- Load Stat collection (statcollection should be available from any script scope)
 require('lib.statcollection')
-statcollection.addStats({
-	modID = 'XXXXXXXXXXXXXXXXXXX' --GET THIS FROM http://getdotastats.com/#d2mods__my_mods
-})
+
+Testing = false --Useful for turning off stat-collection when developing
+
+if not Testing then --Only send stats when not testing
+  statcollection.addStats({
+    modID = 'XXXXXXXXXXXXXXXXXXX' --GET THIS FROM http://getdotastats.com/#d2mods__my_mods
+  })
+end
 
 print( "Example stat collection game mode loaded." )
 
 -- Note: Stats will be sent automatically sent to the server when the game ends
--- plesae see the top of lib/statcollection.lua for more details on how to override this behavior
+-- please see the top of lib/statcollection.lua for more details on how to override this behavior
 
 --------------------------------------------------------------------------------
 -- ACTIVATE
