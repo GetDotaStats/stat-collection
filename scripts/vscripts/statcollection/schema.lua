43deed404c315bce99c4c4361922f3dc6871ba7a
MNoya <martinnoya@gmail.com>
2015-10-21 00:10:33 -0200

    Emptied base schema, added more comments
    Moved rounds to the bottom of the default schema and changed the args to
    a 'isLastRound' parameter that defaults to false in case of
    customSchema:submitRound() calls
    
    Needs testing!

--- scripts/vscripts/statcollection/schema.lua
+++ scripts/vscripts/statcollection/schema.lua
@@ -35,40 +35,15 @@ end
 
 -------------------------------------
 
-function customSchema:submitRound(args)
-    winners = BuildRoundWinnerArray()
-    game = BuildGameArray()
-    players = BuildPlayersArray()
-
-    statCollection:sendCustom({game=game, players=players})
-
-    return {winners = winners, lastRound = false}
-end
-
--------------------------------------
-
-function BuildRoundWinnerArray()
-    local winners = {}
-    local current_winner_team = GameRules.Winner or 0 --You'll need to provide your own way of determining which team won the round
-    for playerID = 0, DOTA_MAX_PLAYERS do
-        if PlayerResource:IsValidPlayerID(playerID) then
-            if not PlayerResource:IsBroadcaster(playerID) then
-                winners[PlayerResource:GetSteamAccountID(playerID)] = (PlayerResource:GetTeam(playerID) == current_winner_team) and 1 or 0
-            end
-        end
-    end
-    return winners
-end
-
--------------------------------------
-
 -- In the statcollection/lib/utilities.lua, you'll find many useful functions to build your schema.
 -- You are also encouraged to call your custom mod-specific functions
 
 -- Returns a table with our custom game tracking.
 function BuildGameArray()
     local game = {}
-    game.rs = GetRoshanKills() -- This is an example of a function that returns how many times roshan was killed
+
+    -- Add game values here as game.someValue = GetSomeGameValue()
+
     return game
 end
 
@@ -85,15 +60,9 @@ function BuildPlayersArray()
                     -- steamID32 required in here
                     steamID32 = PlayerResource:GetSteamAccountID(playerID),
 
-                    -- Example functions of generic stats defined in statcollection/lib/utilities.lua 
-                    -- Keep, delete or change any as needed
-                    ph = GetHeroName(playerID), --Hero by its short name
-                    pk = hero:GetKills(),   --Number of kills of this players hero
-                    pd = hero:GetDeaths(),  --Number of deaths of this players hero
-                    nt = GetNetworth(hero), --Sum of hero gold and item worth
+                    -- Example functions for generic stats are defined in statcollection/lib/utilities.lua
+                    -- Add player values here as someValue = GetSomePlayerValue(),
 
-                    -- Item List
-                    il = GetItemList(hero),
                 })
             end
         end
@@ -115,3 +84,36 @@ end
 if Convars:GetBool('developer') then
     Convars:RegisterCommand("test_schema", function() PrintSchema(BuildGameArray(),BuildPlayersArray()) end, "Test the custom schema arrays", 0)
 end
+
+-------------------------------------
+
+-- If your gamemode is round-based, you can use customSchema:submitRound() at any point of your main game logic code to send a round
+-- If you intend to send rounds, make sure your settings.kv has the 'HAS_ROUNDS' set to true. Each round will send the game and player arrays defined earlier
+-- The round number is incremented internally, lastRound can be marked to notify that the game ended properly
+function customSchema:submitRound(isLastRound)
+
+    local winners = BuildRoundWinnerArray()
+    local game = BuildGameArray()
+    local players = BuildPlayersArray()
+
+    statCollection:sendCustom({game=game, players=players})
+
+    isLastRound = isLastRound or false --If the function is passed with no parameter, default to false.
+    return {winners = winners, lastRound = isLastRound}
+end
+
+-- A list of players marking who won this round
+function BuildRoundWinnerArray()
+    local winners = {}
+    local current_winner_team = GameRules.Winner or 0 --You'll need to provide your own way of determining which team won the round
+    for playerID = 0, DOTA_MAX_PLAYERS do
+        if PlayerResource:IsValidPlayerID(playerID) then
+            if not PlayerResource:IsBroadcaster(playerID) then
+                winners[PlayerResource:GetSteamAccountID(playerID)] = (PlayerResource:GetTeam(playerID) == current_winner_team) and 1 or 0
+            end
+        end
+    end
+    return winners
+end
+
+-------------------------------------
